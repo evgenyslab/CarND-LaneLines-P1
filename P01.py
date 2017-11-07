dce72287d7fe0af59a8b14ca2f6a6cad2e3efd7a
T0182463 <evgeny.nuger@thalesgroup.com>
2017-11-07 08:59:31 -0500

    slight mods

--- P01.py
+++ P01.py
@@ -103,7 +103,7 @@ def weighted_img(img, initial_img, alpha=0.8, beta=1., lmbda=0.):
 
     The result image is computed as follows:
 
-    initial_img * α + img * β + λ
+    initial_img * alpha + img * beta + Lambda
     NOTE: initial_img and img must be the same shape!
     """
     return cv2.addWeighted(initial_img, alpha, img, beta, lmbda)
@@ -121,16 +121,19 @@ def process_image(image):
     # you should return the final output (image where lines are drawn on lanes)
 
     # Create copy of Image
-    output_image = image
+    output_image = image.copy()
     # Convert image to Gray:
     output_image = grayscale(output_image)
+    # Normalize the image:
+    
     # Apply Gaussian Blur to image:
     output_image = gaussian_blur(output_image,7)
     # Run Canny Edge detector:
     edges = canny(output_image, 50, 150)
     # Apply image mask:
     imshape = image.shape
-    vertices = np.array([[(0,imshape[0]),(7*imshape[1]/16, imshape[0]/2), (9*imshape[1]/16, imshape[0]/2), (imshape[1],imshape[0])]], dtype=np.int32)
+    crops = [7.9/16.0, 8.1/16.0, 9/16.0]
+    vertices = np.array([[(0,imshape[0]),(crops[0]*imshape[1], crops[2]*imshape[0]), (crops[1]*imshape[1], crops[2]*imshape[0]), (imshape[1],imshape[0])]], dtype=np.int32)
     output_image = region_of_interest(edges,vertices)
     # run Hough Lines
     #     line_image = np.copy(image)*0
@@ -146,15 +149,27 @@ def process_image(image):
     #     lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
 
     cv2.imshow("test",output_image)
-    cv2.waitKey(10)
+    cv2.waitKey(100)
     return output_image
 
-white_output = 'test_videos_output/solidWhiteRight.mp4'
+
+# fileName = 'solidWhiteRight.mp4'
+# fileName = 'solidYellowLeft.mp4'
+fileName = 'challenge.mp4'
+white_output = 'test_videos_output/' + fileName
 ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
 ## To do so add .subclip(start_second,end_second) to the end of the line below
 ## Where start_second and end_second are integer values representing the start and end of the subclip
 ## You may also uncomment the following line for a subclip of the first 5 seconds
 ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
-clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-%time white_clip.write_videofile(white_output, audio=False)
+clip1 = VideoFileClip('test_videos/' + fileName)
+count =1
+for frames in clip1.iter_frames():
+    cv2.imshow("test",frames)
+    cv2.waitKey(100)
+    count+=1
+print count
+
+# white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+# white_clip.write_videofile(white_output, audio=False)
+cv2.destroyAllWindows()
